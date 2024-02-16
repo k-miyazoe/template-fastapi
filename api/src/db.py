d8f1b66e5fba371922c38224f75ec08848865925
k-miyazoe <nk.miyazoe@gmail.com>
2024-02-16 13:44:23 +0900

    docker db add

--- api/src/db.py
+++ api/src/db.py
@@ -6,10 +6,10 @@ import os
 
 load_dotenv()
 
-host = os.environ.get("DB_HOST")
-db_name = os.environ.get("DB_NAME")
 user = os.environ.get("DB_USER")
 password = os.environ.get("DB_PASSWORD")
+host = os.environ.get("DB_HOST")
+db_name = os.environ.get("DB_NAME")
 
 DATABASE = 'mysql://%s:%s@%s/%s?charset=utf8' % (
     user,
