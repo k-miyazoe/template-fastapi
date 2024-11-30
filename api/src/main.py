7d51c1f874b365230f286ff342176c85b9a2f62d
k-miyazoe <nk.miyazoe@gmail.com>
2024-11-30 23:17:24 +0900

    main.py separation use router

--- api/src/main.py
+++ api/src/main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
-from typing import Union
-from db import session
-from models import TestUserTable, TestUser
-import asyncio
+from sample_parameter import router as sample_parameter_router
+from sample_db_operation import router as sample_db_operation_router
+from sample_async import router as sample_async_router
 
 
 app = FastAPI(
@@ -27,107 +26,13 @@ app.add_middleware(
     allow_headers=['*']
 )
 
-fake_items_db = [{"item_name": "Foo"}, {
-    "item_name": "Bar"}, {"item_name": "Baz"}]
-
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-# クエリパラメータ有り
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip: skip + limit]
-
-# パスパラメータとクエリパラメータ有り
-@app.get("/items/{item_id}")
-async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
-    item = {"item_id": item_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
-
-# 複数のパスパラメータとクエリパラメータ有り
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
-
-# 必須のクエリパラメータ
-@app.get("/items-needy/{item_id}")
-async def read_user_item(item_id: str, needy: str):
-    item = {"item_id": item_id, "needy": needy}
-    return item
+app.include_router(sample_parameter_router)
+app.include_router(sample_db_operation_router)
+app.include_router(sample_async_router)
 
 
-# db接続
-#　ユーザー情報一覧取得
-@app.get("/test_users")
-def get_user_list():
-    users = session.query(TestUserTable).all()
-    session.close()
-    return users
 
-
-# ユーザー情報取得(id指定)
-@app.get("/test_users/{user_id}")
-def get_user(user_id: int):
-    user = session.query(TestUserTable).\
-        filter(TestUserTable.id == user_id).first()
-    session.close()
-    if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
-
-
-# ユーザ情報登録
-@app.post("/test_users")
-def post_user(user: TestUser):
-    try:
-        existing_user = session.query(TestUser).filter(
-            (TestUser.name == user.name) | (TestUser.email == user.email)
-        ).first()
-
-        if existing_user:
-            raise HTTPException(status_code=400, detail="User with same name or email already exists")
-
-        db_test_user = TestUser(name=user.name, email=user.email)
-        session.add(db_test_user)
-        session.commit()
-        return {"message": "User created successfully"}
-    except Exception as e:
-        session.rollback()
-        raise HTTPException(status_code=500, detail="Internal Server Error")
-    finally:
-        session.close()
-
-# ユーザ情報更新
-@app.put("/test_users/{user_id}")
-def put_users(user: TestUser, user_id: int):
-    target_user = session.query(TestUserTable).\
-        filter(TestUserTable.id == user_id).first()
-    target_user.name = user.name
-    target_user.email = user.email
-    session.commit()
-    session.close()
-
-#　非同期処理サンプル
-async def async_task():
-    await asyncio.sleep(3)  # 3秒間の非同期処理をシミュレート
-    return {"message": "Async task completed"}
-
-@app.get("/async_task")
-async def run_async_task():
-    result = await async_task()
-    return result
